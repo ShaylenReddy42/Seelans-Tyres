49be31578adc0fc3c8259505197ff9ea58b4ab4e
Shaylen <shaylenreddy42@gmail.com>
2022-08-19 22:41:52 +0200

    remove the last reference to the old seelanstyresapi client

--- SeelansTyres.Mvc/Components/ShoppingCartSummary.cs
+++ SeelansTyres.Mvc/Components/ShoppingCartSummary.cs
@@ -1,24 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SeelansTyres.Data.Models;
 using SeelansTyres.Mvc.Services;
 
 namespace SeelansTyres.Mvc.Components;
 
 public class ShoppingCartSummary : ViewComponent
 {
-    private readonly HttpClient client;
     private readonly ILogger<ShoppingCartSummary> logger;
     private readonly ICartService cartService;
 
     public ShoppingCartSummary(
         ILogger<ShoppingCartSummary> logger,
-        IHttpClientFactory httpClientFactory,
-        ICartService cartService)
-    {
-        client = httpClientFactory.CreateClient("SeelansTyresAPI");
-        this.logger = logger;
-        this.cartService = cartService;
-    }
+        ICartService cartService) =>
+            (this.logger, this.cartService) = (logger, cartService);
 
     public IViewComponentResult Invoke()
     {
