f24b22ff3e13fc32b29a1a526bf5483da5400177
Shaylen <shaylenreddy42@gmail.com>
2023-01-11 20:01:46 +0200

    resolve that one code smell

--- src/Libraries/SharedLibrarySolution/SeelansTyres.Libraries.Shared/Services/AzureServiceBusPublisher.cs
+++ src/Libraries/SharedLibrarySolution/SeelansTyres.Libraries.Shared/Services/AzureServiceBusPublisher.cs
@@ -21,9 +21,15 @@ public class AzureServiceBusPublisher : IMessagePublisher
     
     public async Task PublishMessageAsync(BaseMessage message, string destination)
     {
+        logger.LogInformation("Configuring a Service Bus Client and Sender for publishing");
+
         await using var serviceBusClient = new ServiceBusClient(configuration["AzureServiceBus:ConnectionString"]);
 
         var serviceBusSender = serviceBusClient.CreateSender(destination);
+
+        logger.LogInformation(
+            "Publishing message to {AzureServiceBusTopic}",
+            destination);
 
         await serviceBusSender.SendMessageAsync(new(JsonSerializer.SerializeToUtf8Bytes(message)));
     }
