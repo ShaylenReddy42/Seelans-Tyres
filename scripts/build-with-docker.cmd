76c590113724d7e1e255ddf3e37061284a1712a0
Shaylen <shaylenreddy42@gmail.com>
2023-01-20 19:14:04 +0200

    nginx: add the health checks ui

--- scripts/build-with-docker.cmd
+++ scripts/build-with-docker.cmd
@@ -11,20 +11,26 @@ ECHO.
 ECHO All the logs will be streamed to this command prompt.
 ECHO This process could take very long the first few times because some images have to be pulled
 ECHO Wait for the message "Kibana is now available (was degraded)" (it'll stop at that point) which means the site and Kibana can be accessed
-ECHO The site is on https://www-local.seelanstyres.com:4000 and Kibana is on http://localhost:5601
 ECHO Press Ctrl+C to stop
 ECHO A message to terminate the console will come up, press N and Enter
-ECHO Then any key to remove all the containers
+ECHO.
+ECHO.
+ECHO URLs
+ECHO -----
+ECHO Health Checks UI    -- https://health-local.seelanstyres.com:4000
+ECHO MVC Frontend        -- https://www-local.seelanstyres.com:4000
+ECHO Identity Service    -- https://id-local.seelanstyres.com:4000
+ECHO.
+ECHO Kibana              -- http://localhost:5601
+ECHO RabbitMQ Management -- http://localhost:15672
 ECHO.
 ECHO.
 
 docker compose -f ../orchestration/docker-compose/docker-compose-all.yml up
 
 ECHO.
-ECHO Continuing will run "docker compose down" to remove all the containers
+ECHO "docker compose down" will be run to remove all the containers
 ECHO.
-
-PAUSE
 
 docker compose -f ../orchestration/docker-compose/docker-compose-all.yml down
 
