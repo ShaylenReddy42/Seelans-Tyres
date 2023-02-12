72cf5110bbf74feb71dcdae79ff6cf02da42567b
Shaylen <shaylenreddy42@gmail.com>
2023-02-12 11:00:21 +0200

    kubernetes: restore the namespace metadata
    shouldn't have removed it in the first place
    
    this is still kept in the default namespace
    
    a small win, the healthchecks ui could connect to the address and order workers, but nothing else
    
    some things just don't make sense, but oh well 🤷‍♂️

--- orchestration/kubernetes/01-secrets/local.cmd
+++ orchestration/kubernetes/01-secrets/local.cmd
@@ -3,7 +3,7 @@
 ECHO Creating secrets
 ECHO.
 
-kubectl create secret generic secrets-global ^
+kubectl create secret generic secrets-global --namespace default ^
     --from-literal=AzureAppConfig__Enabled="%AzureAppConfig__Enabled%" ^
     --from-literal=AzureAppConfig__ConnectionString="%AzureAppConfig__ConnectionString%" ^
     --from-literal=AppInsights__Enabled="%AppInsights__Enabled%" ^
@@ -13,22 +13,22 @@ kubectl create secret generic secrets-global ^
     --from-literal=EmailCredentials__Email="%EmailCredentials__Email%" ^
     --from-literal=EmailCredentials__Password="%EmailCredentials__Password%"
 
-kubectl create secret tls tls-for-health-local ^
+kubectl create secret tls tls-for-health-local --namespace default ^
     --cert "%~dp0health-local.seelanstyres.com.crt" ^
     --key  "%~dp0health-local.seelanstyres.com.key"
 
-kubectl create secret tls tls-for-id-local ^
+kubectl create secret tls tls-for-id-local --namespace default ^
     --cert "%~dp0id-local.seelanstyres.com.crt" ^
     --key  "%~dp0id-local.seelanstyres.com.key"
 
-kubectl create secret tls tls-for-www-local ^
+kubectl create secret tls tls-for-www-local --namespace default ^
     --cert "%~dp0www-local.seelanstyres.com.crt" ^
     --key  "%~dp0www-local.seelanstyres.com.key"
 
-kubectl create secret tls tls-for-kibana-local ^
+kubectl create secret tls tls-for-kibana-local --namespace default ^
     --cert "%~dp0kibana-local.seelanstyres.com.crt" ^
     --key  "%~dp0kibana-local.seelanstyres.com.key"
 
-kubectl create secret tls tls-for-rabbitmq-local ^
+kubectl create secret tls tls-for-rabbitmq-local --namespace default ^
     --cert "%~dp0rabbitmq-local.seelanstyres.com.crt" ^
     --key  "%~dp0rabbitmq-local.seelanstyres.com.key"
