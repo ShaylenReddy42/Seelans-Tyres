48ae1958cdb7ce3cf1ab057f9d14a0e087660dc3
Shaylen Reddy <shaylenreddy42@gmail.com>
2024-05-29 21:22:52 +0200

    oops, forgot elasticsearch
    [skip ci]

--- src/Libraries/SharedLibrarySolution/SeelansTyres.Libraries.Shared/Abstractions/HealthChecks.cs
+++ src/Libraries/SharedLibrarySolution/SeelansTyres.Libraries.Shared/Abstractions/HealthChecks.cs
@@ -36,6 +36,7 @@ public static class HealthChecks
             healthChecks
                 .AddElasticsearch(
                     elasticsearchUri: healthChecksModel.ElasticsearchUrl,
+                    name: "Elasticsearch",
                     failureStatus: HealthStatus.Degraded,
                     timeout: TimeSpan.FromSeconds(1.5));
         }
